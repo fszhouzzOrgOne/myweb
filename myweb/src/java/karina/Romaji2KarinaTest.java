89fa7b0c0eab9232c5f5afa48672143ad5418490
fszhouzz <fszhouzz@qq.com>
2018-01-24 00:17:15 +0800

    日文nn

--- myweb/src/java/karina/Romaji2KarinaTest.java
+++ myweb/src/java/karina/Romaji2KarinaTest.java
@@ -19,6 +19,8 @@ public class Romaji2KarinaTest {
         System.out.println(getKarinaFromRomaji("danya"));
         System.out.println(getKarinaFromRomaji("nina"));
         System.out.println(getKarinaFromRomaji("shin"));
+        System.out.println(getKarinaFromRomaji("anou"));
+        System.out.println(getKarinaFromRomaji("gomennasai"));
     }
 
     /** 片假名的長音 */
@@ -471,9 +473,10 @@ public class Romaji2KarinaTest {
             List<String> resTmp3 = new ArrayList<String>();
             for (String line : resTmp) {
                 resTmp3.add(line);
-                // 開頭的n，不換
+                // 開頭的n，不換；如果有兩個nChar相連，換一下後一個为n
                 String suffix = line.substring(1);
-                resTmp3.add(line.substring(0, 1) + suffix.replaceAll("n", nChar));
+                resTmp3.add(
+                        (line.substring(0, 1) + suffix.replaceAll("n", nChar)).replaceAll(nChar + nChar, nChar + "n"));
             }
             resTmp = resTmp3;
         }
