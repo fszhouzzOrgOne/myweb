d004087c24910e2d1198b5c607840ddb60b9c56a
zhouzizhao <fszhouzz@qq.com>
2018-01-25 17:36:49 +0800

    11

--- myweb/src/java/cangjie/java/Cj01SQLiteTest.java
+++ myweb/src/java/cangjie/java/Cj01SQLiteTest.java
@@ -65,7 +65,7 @@ public class Cj01SQLiteTest {
 
         // 互斥的版本選擇
         boolean edition1 = false; // 1版本默認字體 同2
-        boolean edition2 = true; // 2版本自定義字體 471356
+        boolean edition2 = true; // 2版本自定義字體 471358
         boolean edition3 = false; // 版本倉頡三 177361
         boolean edition35 = false; // 版本倉頡三五 164901 ANSI 105618
         boolean edition35only5 = false; // 版本倉頡三五只要五代 159268 ansi 103934
