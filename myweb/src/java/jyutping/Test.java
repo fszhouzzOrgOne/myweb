9ea83d6841753a6ca40fa0a7cba27f252083dfc0
zhouzizhao <fszhouzz@qq.com>
2018-02-06 15:53:41 +0800

    jyutping/jyutping-more.txt

--- myweb/src/java/jyutping/Test.java
+++ myweb/src/java/jyutping/Test.java
@@ -1,13 +1,13 @@
 package jyutping;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
 
 import cangjie.java.Cj00AllInOneTest;
-import cangjie.java.PhraseTest;
 import cangjie.java.util.IOUtils;
 
 public class Test {
@@ -15,19 +15,13 @@ public class Test {
     public static String mbsBaseDir = "src\\java\\jyutping\\";
 
     public static void main(String[] args) throws Exception {
-        // List<String> res = readMbCode_char_char(mbsBaseDir +
-        // "粤语发声字典081217.txt");
-        // IOUtils.writeFile(mbsBaseDir + "粤语发声字典081217_2.txt", res);
-
         mergeMbs();
 
         // 編碼粵拼的詞組
         String mbFile = mbsBaseDir + "jyutping-all.txt";
         String jyutPhrase = mbsBaseDir + "jyutping-phrases.txt";
-        
-        String srcPhraseFile = Cj00AllInOneTest.phraseOriginFile;
-        PhraseTest.uniqueOrderPhraseFile();
-        List<String> phrases = getJyutpingPhrase(mbFile, srcPhraseFile);
+
+        List<String> phrases = getJyutpingPhrase();
         IOUtils.writeFile(jyutPhrase, phrases);
 
         // 粵拼碼表整合
@@ -41,7 +35,7 @@ public class Test {
                         .replace("5", "qqqqq").replace("6", "qqqqqq"));
             }
         }
-        // jyutAllInOne.addAll(jyutPhrases);
+        jyutAllInOne.addAll(jyutPhrases);
         IOUtils.writeFile(Cj00AllInOneTest.jyutping20000, jyutAllInOne);
     }
 
@@ -55,80 +49,24 @@ public class Test {
      * @return
      * @throws Exception
      */
-    private static List<String> getJyutpingPhrase(String mbFile, String srcPhraseFile) throws Exception {
-        List<String> mb = IOUtils.readLines(mbFile);
-        Map<String, List<String>> charJyutpMap = new HashMap<String, List<String>>();
-        for (String line : mb) {
-            String[] part = line.split(" ");
-            String code = part[0];
-            String cha = part[1];
-            List<String> list = charJyutpMap.get(cha);
-            if (null == list) {
-                list = new ArrayList<String>();
-            }
-            String codeSim = code.replaceAll("[0-9]+", "");
-            if (!list.contains(codeSim)) {
-                list.add(codeSim);
-            }
-            charJyutpMap.put(cha, list);
-        }
+    private static List<String> getJyutpingPhrase() throws Exception {
+        String path = mbsBaseDir + File.separator + "praseGet" + File.separator;
+        List<String> phrs = new ArrayList<String>();
+        phrs.addAll(IOUtils.readLines(path + "粤语发声字典中所有的字音詞3结果.txt"));
+        phrs.addAll(IOUtils.readLines(path + "粵語詞彙1.txt"));
 
-        // 編碼詞組
-        System.out.println("開始編碼詞組：");
-        List<String> phrs = IOUtils.readLines(srcPhraseFile);
-        List<String> res = new ArrayList<String>();
-        outerFor: for (String phr : phrs) {
-            if (null != phr && phr.trim().length() > 0) {
-                char[] chars = phr.trim().toCharArray();
-                List<List<String>> lists = new ArrayList<List<String>>();
-                for (Character cha : chars) {
-                    List<String> charCodes = charJyutpMap.get(cha.toString());
-                    if (null == charCodes) {
-                        continue outerFor;
-                    } else {
-                        lists.add(charCodes);
-                    }
-                }
+        Set<String> res = new HashSet<String>();
+        for (String phr : phrs) {
+            if (phr.contains(" ")) {
+                String parts[] = phr.split(" ");
 
-                // 保存生成的所有編碼
-                List<String> tempRes = new ArrayList<String>();
-                for (List<String> oneList : lists) {
-                    if (tempRes.isEmpty()) {
-                        tempRes.addAll(oneList);
-                    } else if (oneList.size() > 1) {
-                        // 加倍，oneList有幾個元素就加多少倍
-                        List<List<String>> tempRes2 = new ArrayList<List<String>>();
-                        for (int i = 0; i < oneList.size(); i++) {
-                            tempRes2.add(new ArrayList<String>(tempRes));
-                        }
-                        // 第一份，都加上第一個編碼；第N份，都加上第N個編碼……
-                        for (int i = 0; i < oneList.size(); i++) {
-                            List<String> temp = tempRes2.get(i);
-                            List<String> tempNew = new ArrayList<String>();
-                            for (String str : temp) {
-                                tempNew.add(str + oneList.get(i));
-                            }
-                            tempRes2.set(i, tempNew);
-                        }
-                        tempRes.clear();
-                        for (List<String> ls : tempRes2) {
-                            tempRes.addAll(ls);
-                        }
-                    } else {
-                        List<String> tempRes2 = new ArrayList<String>();
-                        for (String t : tempRes) {
-                            tempRes2.add(t + oneList.get(0));
-                        }
-                        tempRes = tempRes2;
-                    }
-                }
-                for (String code : tempRes) {
-                    res.add(code + " " + phr);
+                String line = parts[1].replaceAll("[0-9]", "") + " " + parts[0];
+                if (!res.contains(line)) {
+                    res.add(line);
                 }
             }
         }
-        System.out.println("結束編碼詞組。");
-        return res;
+        return new ArrayList<String>(res);
     }
 
     /**
@@ -153,7 +91,7 @@ public class Test {
     }
 
     /**
-     * 合併兩個粵拼的碼表
+     * 合併兩個粵拼的碼表：jyutping-github.txt、jyutping-dict.txt
      * 
      * @author fszhouzz@qq.com
      * @time 2017年12月14日 下午10:26:45
@@ -174,9 +112,11 @@ public class Test {
         list3.add("qqqqq ˨˧");
         list3.add("qqqqqq ˨˨");
         list3.add("qqqqqq ˨");
+        List<String> list4 = IOUtils.readLines(mbsBaseDir + "jyutping-more.txt");
         List<String> listall = new ArrayList<String>(list1);
         listall.addAll(list2);
         listall.addAll(list3);
+        listall.addAll(list4);
         Collections.sort(listall);
         IOUtils.writeFile(mbsBaseDir + "jyutping-all.txt", listall);
         IOUtils.uniqueCodeFile(mbsBaseDir + "jyutping-all.txt");
