0e740b4c3293ba278c201b717cee3521392f5f74
fszhouzz <fszhouzz@qq.com>
2018-05-16 18:17:34 +0800

    time/天文历/寿星天文历Java封装整理版.txt

--- myweb/src/java/time/天文历/Test.java
+++ myweb/src/java/time/天文历/Test.java
@@ -2,10 +2,11 @@ package time.天文历;
 
 /**
  * JulianDate.java、 LunarCalendar.java、 LunarConstant.java、 LunarDate.java<br/>
- * 四個類來自：https://download.csdn.net/download/wangpeng047/7755661
+ * 四個類來自：https://download.csdn.net/download/wangpeng047/7755661<br/>
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("aaa");
+        LunarCalendar lc = new LunarCalendar();
+        System.out.println(lc.getGanZhiDateString());
     }
 }
