61c3118b456b632824b50f60247d666535f30347
fszhouzz <fszhouzz@qq.com>
2018-05-16 18:03:31 +0800

    csdn下載了個天文曆java版。

--- myweb/src/java/time/天文历/Test.java
+++ myweb/src/java/time/天文历/Test.java
@@ -1,5 +1,9 @@
 package time.天文历;
 
+/**
+ * JulianDate.java、 LunarCalendar.java、 LunarConstant.java、 LunarDate.java<br/>
+ * 四個類來自：https://download.csdn.net/download/wangpeng047/7755661
+ */
 public class Test {
     public static void main(String[] args) {
         System.out.println("aaa");
