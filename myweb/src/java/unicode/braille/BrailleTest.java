26aa6c5c6565deee7914f17747161dc8a1e8d24f
fszhouzz <fszhouzz@qq.com>
2018-11-02 23:27:42 +0800

    564646466

--- myweb/src/java/unicode/braille/BrailleTest.java
+++ myweb/src/java/unicode/braille/BrailleTest.java
@@ -25,6 +25,179 @@ public class BrailleTest {
     private static Map<String, String> codeCharMap = new HashMap<String, String>();
     private static Map<String, List<String>> codeCharMapEn = new HashMap<String, List<String>>();
 
+    public static void main(String[] args) {
+        String input = "wqwe";
+        System.out.println(input);
+        List<String> cands = getCandidatesByInput(input);
+        System.out.println(cands);
+    }
+
+    public static List<String> getCandidatesByInput(String input) {
+        List<String> resList = new ArrayList<String>();
+        List<String> parts = resolveInputParts(input);
+        System.out.println(parts);
+        String fullCode = "";
+        String reses = "";
+        for (String part : parts) {
+            String one = part.charAt(0) + "";
+            String code = null;
+            if (keyMap1.keySet().contains(one)) {
+                code = resolveNumTrueCode(part);
+            } else if (keyMap2.keySet().contains(one)) {
+                code = resolveImageTrueCode(part);
+            }
+            fullCode += code;
+            reses += codeCharMap.get(code);
+        }
+        resList.add(reses);
+        List<String> resEn = codeCharMapEn.get(fullCode);
+        if (null != resEn) {
+            resList.addAll(resEn);
+        }
+        return resList;
+    }
+
+    public static String resolveImageTrueCode(String part) {
+        String code1 = keyMap2.get(part.charAt(0) + "");
+        if (code1.length() == 8) {
+            return codeCharMap.get(code1);
+        }
+        String code2 = keyMap2.get(part.charAt(1) + "");
+        List<String> codes = new ArrayList<String>(
+                Arrays.asList("0", "0", "0", "0", "0", "0", "0", "0"));
+        codes.set(0, code1.charAt(0) + "");
+        codes.set(1, code1.charAt(2) + "");
+        codes.set(2, code2.charAt(0) + "");
+        codes.set(3, code1.charAt(1) + "");
+        codes.set(4, code1.charAt(3) + "");
+        codes.set(5, code2.charAt(1) + "");
+        codes.set(6, code2.charAt(2) + "");
+        codes.set(7, code2.charAt(3) + "");
+        String codex = "";
+        for (String one : codes) {
+            codex += one;
+        }
+        return codex;
+    }
+
+    public static String resolveNumTrueCode(String part) {
+        List<String> codes = new ArrayList<String>(
+                Arrays.asList("0", "0", "0", "0", "0", "0", "0", "0"));
+        for (int i = 0; i < part.length(); i++) {
+            int one = keyMap1.get(part.charAt(i) + "");
+            codes.set(one - 1, "1");
+        }
+        String codex = "";
+        for (String one : codes) {
+            codex += one;
+        }
+        return codex;
+    }
+
+    public static List<String> resolveInputParts(String input) {
+        boolean isNum = false;
+        List<String> parts = new ArrayList<String>();
+        String part = "";
+        for (int i = 0; i < input.length(); i++) {
+            String one = input.charAt(i) + "";
+            if (keyMap1.keySet().contains(one)) {
+                if (!isNum && part.length() > 0) {
+                    parts.add(part + (part.length() > 1 ? "" : "a"));
+                    part = "";
+                }
+                isNum = true;
+                int oneInt = keyMap1.get(one);
+                if ("q".equals(one)) {
+                    if (part.length() > 0) {
+                        parts.add(part);
+                        part = "";
+                    }
+                } else {
+                    if (part.length() > 0) {
+                        String two = part.charAt(part.length() - 1) + "";
+                        int twoInt = keyMap1.get(two);
+                        if (twoInt >= oneInt) {
+                            parts.add(part);
+                            part = one;
+                        } else {
+                            part += one;
+                        }
+                    } else {
+                        part = one;
+                    }
+                }
+            } else if (keyMap2.keySet().contains(one)) {
+                if (isNum && part.length() > 0) {
+                    parts.add(part);
+                    part = "";
+                }
+                isNum = false;
+                if ("p".equals(one)) {
+                    if (part.length() > 0) {
+                        parts.add(part + (part.length() > 1 ? "" : "a"));
+                        part = "";
+                    }
+                    parts.add("p");
+                } else {
+                    if (part.length() > 0) {
+                        parts.add(part + one);
+                        part = "";
+                    } else if (i == input.length() - 1) {
+                        parts.add(one + "a");
+                        part = "";
+                    } else {
+                        part = one;
+                    }
+                }
+            }
+        }
+        if (part.length() > 0) {
+            parts.add(part);
+            part = "";
+        }
+        return parts;
+    }
+
+    public static List<String> getBraillePatterns() {
+        int start = 0x2800;
+        int end = 0x28FF;
+        List<String> lines = new ArrayList<String>();
+        for (int i = start; i <= end; i++) {
+            String str = UnicodeConvertUtil.getStringByUnicode(i);
+            String name = Character.getName(i);
+            String code = name.contains("-")
+                    ? name.substring(name.indexOf("-") + 1) : "00000000";
+            String line = toFullCode(code) + " " + str;
+            lines.add(line);
+        }
+        return lines;
+    }
+
+    public static String toFullCode(String code) {
+        int len = 8;
+        if (code.length() == len) {
+            return code;
+        }
+        String res = "";
+        for (int i = 0; i < code.length(); i++) {
+            String one = code.charAt(i) + "";
+            int oneInt = Integer.parseInt(one);
+            if ((i + 1) != oneInt) {
+                int start = (i == 0) ? 1
+                        : Integer.parseInt(code.charAt(i - 1) + "") + 1;
+                int end = oneInt - 1;
+                for (int j = start; j <= end; j++) {
+                    res += "0";
+                }
+            }
+            res += one;
+        }
+        while (res.length() < len) {
+            res += "0";
+        }
+        return res;
+    }
+
     static {
         keyMap1.put("q", 0);
         keyMap1.put("w", 1);
@@ -470,169 +643,5 @@ public class BrailleTest {
             }
             codeCharMapEn.put(key, val);
         }
-    }
-
-    public static void main(String[] args) {
-        List<String> parts = resolveInputParts("w");
-        System.out.println(parts);
-        String fullCode = "";
-        String reses = "";
-        for (String part : parts) {
-            String one = part.charAt(0) + "";
-            String code = null;
-            if (keyMap1.keySet().contains(one)) {
-                code = resolveNumTrueCode(part);
-            } else if (keyMap2.keySet().contains(one)) {
-                code = resolveImageTrueCode(part);
-            }
-            fullCode += code;
-            reses += codeCharMap.get(code);
-        }
-        System.out.println(reses);
-        List<String> resEn = codeCharMapEn.get(fullCode);
-        if (null != resEn) {
-            System.out.println(resEn);
-        }
-    }
-
-    public static String resolveImageTrueCode(String part) {
-        String code1 = keyMap2.get(part.charAt(0) + "");
-        if (code1.length() == 8) {
-            return codeCharMap.get(code1);
-        }
-        String code2 = keyMap2.get(part.charAt(1) + "");
-        List<String> codes = new ArrayList<String>(
-                Arrays.asList("0", "0", "0", "0", "0", "0", "0", "0"));
-        codes.set(0, code1.charAt(0) + "");
-        codes.set(1, code1.charAt(2) + "");
-        codes.set(2, code2.charAt(0) + "");
-        codes.set(3, code1.charAt(1) + "");
-        codes.set(4, code1.charAt(3) + "");
-        codes.set(5, code2.charAt(1) + "");
-        codes.set(6, code2.charAt(2) + "");
-        codes.set(7, code2.charAt(3) + "");
-        String codex = "";
-        for (String one : codes) {
-            codex += one;
-        }
-        return codex;
-    }
-
-    public static String resolveNumTrueCode(String part) {
-        List<String> codes = new ArrayList<String>(
-                Arrays.asList("0", "0", "0", "0", "0", "0", "0", "0"));
-        for (int i = 0; i < part.length(); i++) {
-            int one = keyMap1.get(part.charAt(i) + "");
-            codes.set(one - 1, "1");
-        }
-        String codex = "";
-        for (String one : codes) {
-            codex += one;
-        }
-        return codex;
-    }
-
-    public static List<String> resolveInputParts(String input) {
-        boolean isNum = false;
-        List<String> parts = new ArrayList<String>();
-        String part = "";
-        for (int i = 0; i < input.length(); i++) {
-            String one = input.charAt(i) + "";
-            if (keyMap1.keySet().contains(one)) {
-                if (!isNum && part.length() > 0) {
-                    parts.add(part + (part.length() > 1 ? "" : "a"));
-                    part = "";
-                }
-                isNum = true;
-                int oneInt = keyMap1.get(one);
-                if ("q".equals(one)) {
-                    if (part.length() > 0) {
-                        parts.add(part);
-                        part = "";
-                    }
-                } else {
-                    if (part.length() > 0) {
-                        String two = part.charAt(part.length() - 1) + "";
-                        int twoInt = keyMap1.get(two);
-                        if (twoInt >= oneInt) {
-                            parts.add(part);
-                            part = one;
-                        } else {
-                            part += one;
-                        }
-                    } else {
-                        part = one;
-                    }
-                }
-            } else if (keyMap2.keySet().contains(one)) {
-                if (isNum && part.length() > 0) {
-                    parts.add(part);
-                    part = "";
-                }
-                isNum = false;
-                if ("p".equals(one)) {
-                    if (part.length() > 0) {
-                        parts.add(part + (part.length() > 1 ? "" : "a"));
-                        part = "";
-                    }
-                    parts.add("p");
-                } else {
-                    if (part.length() > 0) {
-                        parts.add(part + one);
-                        part = "";
-                    } else if (i == input.length() - 1) {
-                        parts.add(one + "a");
-                        part = "";
-                    } else {
-                        part = one;
-                    }
-                }
-            }
-        }
-        if (part.length() > 0) {
-            parts.add(part);
-            part = "";
-        }
-        return parts;
-    }
-
-    public static List<String> getBraillePatterns() {
-        int start = 0x2800;
-        int end = 0x28FF;
-        List<String> lines = new ArrayList<String>();
-        for (int i = start; i <= end; i++) {
-            String str = UnicodeConvertUtil.getStringByUnicode(i);
-            String name = Character.getName(i);
-            String code = name.contains("-")
-                    ? name.substring(name.indexOf("-") + 1) : "00000000";
-            String line = toFullCode(code) + " " + str;
-            lines.add(line);
-        }
-        return lines;
-    }
-
-    public static String toFullCode(String code) {
-        int len = 8;
-        if (code.length() == len) {
-            return code;
-        }
-        String res = "";
-        for (int i = 0; i < code.length(); i++) {
-            String one = code.charAt(i) + "";
-            int oneInt = Integer.parseInt(one);
-            if ((i + 1) != oneInt) {
-                int start = (i == 0) ? 1
-                        : Integer.parseInt(code.charAt(i - 1) + "") + 1;
-                int end = oneInt - 1;
-                for (int j = start; j <= end; j++) {
-                    res += "0";
-                }
-            }
-            res += one;
-        }
-        while (res.length() < len) {
-            res += "0";
-        }
-        return res;
     }
 }
