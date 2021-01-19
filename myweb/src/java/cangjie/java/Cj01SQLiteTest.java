38298fcce64f5a497ba30b5e661ce38ca129f1a3
fszhouzz <fszhouzz@qq.com>
2021-01-20 01:23:45 +0800

    版本五代 178742

--- myweb/src/java/cangjie/java/Cj01SQLiteTest.java
+++ myweb/src/java/cangjie/java/Cj01SQLiteTest.java
@@ -132,10 +132,10 @@ public class Cj01SQLiteTest {
 
     public static void main(String args[]) throws Exception {
         // 互斥的版本選擇
-        boolean editionAll = true; // 版本自定義字體 514145
+        boolean editionAll = false; // 版本自定義字體 514145
         boolean edition3 = false; // 版本倉頡三 164682
         boolean edition35 = false; // 版本倉頡三五 185549
-        boolean edition5 = false; // 版本五代 178043
+        boolean edition5 = true; // 版本五代 178742
         boolean editionSpecial5 = false; // 特别五代，粵語不要，換吳語 188760
         boolean edition6 = false; // 版本六 275759
         boolean edition62 = false; // 版本六，帶詞組
